12a361c1654d94651ead3a445f4d56b50a2d39db
Serg <bsi_acs@mail.ru>
2020-12-02 16:19:04 +0300

    Добавьте файлы проекта.

---
+++ DI_Test_ASP/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using DI_Test_ASP.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_Test_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        IMessageSender _messageSender;
+        TimeService _timeService;
+
+        public HomeController(IMessageSender messageSender, TimeService timeService)
+        {
+            _messageSender = messageSender;
+            _timeService = timeService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = _timeService.GetTime() + " " + _messageSender.Send();
+
+            return View();
+        }
+    }
+}
---
+++ DI_Test_ASP/Startup.cs
@@ -0,0 +1,38 @@
+using DI_Test_ASP.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_Test_ASP
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddTransient<IMessageSender, EmailMessageSender>();
+            services.AddTransient<TimeService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{Action=Index}");
+            });
+        }
+    }
+}
